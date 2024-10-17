8c6ab6b96229cb46241f0b76957cddf22b7b7f3e
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-10-17 14:33:32 +0530

    Added Skeleton Routes

--- index.js
+++ index.js
@@ -5,32 +5,39 @@ const jwt = require('jsonwebtoken');
 
 app.use(express.json());
 
+const db =  mongoose.connect("mongodb+srv://higyanaranjanpatra:db123@cluster0.2svfp.mongodb.net/");
+db.then(()=>{
+    console.log("Successfully connected to DB")
+})
 
-
-app.post("/signup",(req,res)=>{
+app.post("/user/signup",(req,res)=>{
     res.json({
         message:"Signup Page"
     })
 });
 
 
-app.post("/signin",(req,res)=>{
+app.post("/user/signin",(req,res)=>{
+    res.json({
+        message:"Signin Page"
+    })
+});
+
+app.post("/course/purchase",(req,res)=>{
+    res.json({
+        purchse:"page"
+    })
+})
+
+
+app.get("/user/purchases",(req,res)=>{
     res.json({
         message:"Signin Page"
     })
 });
 
 
-app.post("/purchase",(req,res)=>{
-    res.json({
-        message:"Purchase Page"
-    })
-});
-
-
-
-
-app.get("corse",(req,res)=>{
+app.get("/courses",(req,res)=>{
     res.json({
         message:"Corse Page"
     })
