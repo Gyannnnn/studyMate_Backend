75497950341c0c5a50a46751a9aedec5f2ea821e
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-10-18 19:23:18 +0530

    -> Added Admin Signup code

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,44 @@
 const { Router } = require("express");
-const { admin } = require("../database/db");
+const { adminModel } = require("../database/db");
+const { z } = require('zod');
+const bcrypt = require('bcrypt')
 
  const adminRouter = Router();
 
-adminRouter.post("/signup", (req, res) => {
-    res.json({
-        message: "Signup Page"
-    })
+adminRouter.post("/signup", async(req, res) => {
+    const requiredBody = z.object({
+        email: z.string().email(),
+        password: z.string().min(3).max(10),
+        firstName: z.string().min(5).max(20),
+        lastName: z.string().min(4).max(10) 
+    });
+
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+    if (!parsedDataWithSuccess.success) {
+        return res.status(400).json({
+            message: "Incorrect Format",
+            err: parsedDataWithSuccess.error
+        })
+    }
+    const { email, password, firstName, lastName } = req.body;
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newAdmin = await adminModel.create({
+            email,
+            password: hashedPassword,
+            firstName,
+            lastName
+        });
+        return res.status(200).json({
+            message: "Admin Created Successfully",
+            "user": newAdmin
+        })
+
+    } catch (error) {
+        res.status(502).json({
+            "Error In Signing Up": error.message
+        })
+    }
 });
 
 
